2fa21eddbca35a7298f1553b059cbdfd86df2549
Shaswata Roy <shaswataroy1234@gmail.com>
2024-08-14 01:00:37 +0530

    upadate the files

--- src/app.js
+++ src/app.js
@@ -1,17 +1,19 @@
-import express from 'express';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(cors({
-    origin : process.env.CORS_ORIGIN
-}));
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+  })
+);
 
 //using middleware to handle json
-app.use(express.json({limit : "20kb"}))
-app.use(express.urlencoded({extended:true, limit : "20kb"}))
-app.use(express.static("public"))
-app.use(cookieParser())
+app.use(express.json({ limit: "20kb" }));
+app.use(express.urlencoded({ extended: true, limit: "20kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
-export {app}
+export { app };
---
+++ src/models/user.model.js
@@ -0,0 +1,92 @@
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, // Cloudinary Url
+      required: true,
+    },
+    coverimage: {
+      type: String,
+    },
+    watchHistory: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: true,
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+// Hash Password before saving
+userSchema.pre("save", async function(next){
+    // If password has already been not modified before saving then rewtuen
+    if(!this.isModified("password")) return next();
+
+    this.password  = bcrypt.hash(this.password)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function(password){
+  return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAcessToken = function(){
+  jwt.sign(
+    {
+      _id : this._id,
+      email : this.email,
+      username : this.username,
+      fullname : this.fullname,
+    },
+    process.env.ACESS_TOKEN_SECRET,
+    { 
+      expiresIn : ACESS_TOKEN_EXPIRY
+    }
+  )
+}
+
+userSchema.methods.generateRefreshToken = function(){
+  jwt.sign(
+    {
+      _id : this._id,
+    },
+    process.env.ACESS_TOKEN_SECRET,
+    { 
+      expiresIn : ACESS_TOKEN_EXPIRY
+    }
+  )
+}
+
+export const User = mongoose.model("User", userSchema);
