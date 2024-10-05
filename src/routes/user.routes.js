740238adfed7a8674912c2c944fb20d39ee597f5
Shaswata Roy <shaswataroy1234@gmail.com>
2024-10-05 16:19:23 +0530

    Add coment, like, playlist and model

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -37,7 +37,7 @@ router.route("/change-password").post(verifyJWT, chageCurrentPassword)
 router.route("/current-user").get(verifyJWT, getCurrentUser)
 router.route("/update-account").patch(verifyJWT, updateAccountDetails)
 router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
-router.route("/cover-image").patch(verifyJWT, upload.single("/coverImage"), updateUserCoverimage)
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverimage)
 
 router.route("/c/:username").get(verifyJWT, getUserChnnelProfile)
 router.route("/history").get(verifyJWT, getWatchHistory)
